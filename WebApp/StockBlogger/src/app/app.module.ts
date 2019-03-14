708743de872b72407052fdbb209fc29fa5393819
James Griggs <jamezgriggz13@hotmail.com>
2019-03-14 13:37:24 -0600

    Generated login/register components
    Barebones components, NO LOGIC. Use these as the building blocks for the login and register account functionalities

--- WebApp/StockBlogger/src/app/app.module.ts
+++ WebApp/StockBlogger/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './account/register/register.component';
+import { LoginComponent } from './account/login/login.component';
+import { ResetpasswordComponent } from './account/resetpassword/resetpassword.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ResetpasswordComponent
   ],
   imports: [
     BrowserModule,
