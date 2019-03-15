25c3f25762691e9e4ae7fba05447a824d9fd2cc6
caleb <cschnarr85@gmail.com>
2019-03-15 00:47:16 -0600

    DataConnection to stockblogger db ran on localhost

---
+++ Client-Server/DataConnection.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataConnection {
+	
+	private static final String USERNAME = "dbuser";
+	private static final String PASSWORD = "user";
+	private static final String CONN = "jdbc:mysql://localhost/stockblogger";
+	
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(CONN, USERNAME, PASSWORD);
+	}
+
+}
