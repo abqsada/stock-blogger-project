78a3aff4df29c93d5f84d0caeb7ecc30375110ee
caleb <cschnarr85@gmail.com>
2019-03-19 21:10:34 -0600

    stored proc
    Added a few stored procedures to the DB creation script

--- stockbloggerDB.sql
+++ stockbloggerDB.sql
@@ -28,7 +28,7 @@ DROP TABLE IF EXISTS `stockblogger`.`users` ;
 CREATE TABLE IF NOT EXISTS `stockblogger`.`users` (
   `user_id` INT(11) NOT NULL AUTO_INCREMENT,
   `user_name` VARCHAR(45) NOT NULL,
-  `date_user_joined` DATE NULL DEFAULT NULL,
+  `date_user_joined` DATE NOT NULL,
   `password` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`user_id`))
 ENGINE = InnoDB
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS `stockblogger`.`posts` (
   `user_id` INT(11) NOT NULL,
   `title` VARCHAR(75) NOT NULL,
   `body` VARCHAR(5000) NOT NULL,
-  `post_date` DATE NULL DEFAULT NULL,
+  `post_date` DATE NOT NULL,
   PRIMARY KEY (`post_id`),
   INDEX `post to user_idx` (`user_id` ASC) VISIBLE,
   CONSTRAINT `post to user`
@@ -66,8 +66,8 @@ CREATE TABLE IF NOT EXISTS `stockblogger`.`comments` (
   `comment_id` INT(11) NOT NULL AUTO_INCREMENT,
   `post_id` INT(11) NOT NULL,
   `user_id` INT(11) NOT NULL,
-  `body` VARCHAR(1000) NULL DEFAULT NULL,
-  `comment_date` DATE NULL DEFAULT NULL,
+  `body` VARCHAR(1000) NOT NULL,
+  `comment_date` DATE NOT NULL,
   PRIMARY KEY (`comment_id`),
   INDEX `comment to post_idx` (`post_id` ASC) VISIBLE,
   INDEX `comment to user_idx` (`user_id` ASC) VISIBLE,
@@ -197,6 +197,91 @@ VALUES
 1,
 'some text',
 DATE '2016-01-25');
+
+USE `stockblogger`;
+DROP procedure IF EXISTS `add_user`;
+
+DELIMITER $$
+USE `stockblogger`$$
+CREATE PROCEDURE `add_user` (IN USER_NAME_INPUT VARCHAR(45),IN DATE_JOINED_INPUT DATE,IN PASSWORD_INPUT VARCHAR(45))
+BEGIN
+
+INSERT INTO `stockblogger`.`users`
+(`user_name`,
+`date_user_joined`,
+`password`)
+VALUES
+(USER_NAME_INPUT,
+DATE_JOINED_INPUT,
+PASSWORD_INPUT);
+
+END$$
+
+DELIMITER ;
+
+USE `stockblogger`;
+DROP procedure IF EXISTS `add_post`;
+
+DELIMITER $$
+USE `stockblogger`$$
+CREATE PROCEDURE `add_post` (IN USER_ID_INPUT INT,IN TITLE_INPUT VARCHAR(75),IN POST_BODY_INPUT VARCHAR(5000),IN POST_DATE_INPUT DATE)
+BEGIN
+
+INSERT INTO `stockblogger`.`posts`
+(`user_id`,
+`title`,
+`body`,
+`post_date`)
+VALUES
+(USER_ID_INPUT,
+TITLE_INPUT,
+POST_BODY_INPUT,
+POST_DATE_INPUT);
+
+END$$
+
+DELIMITER ;
+
+USE `stockblogger`;
+DROP procedure IF EXISTS `add_comment`;
+
+DELIMITER $$
+USE `stockblogger`$$
+CREATE PROCEDURE `add_comment`(IN POST_ID_INPUT INT,IN USER_ID_INPUT INT,IN COMMENT_BODT_INPUT VARCHAR(1000),IN COMMENT_DATE_INPUT DATE)
+BEGIN
+
+INSERT INTO `stockblogger`.`comments`
+(`post_id`,
+`user_id`,
+`body`,
+`comment_date`)
+VALUES
+(POST_ID_INPUT,
+USER_ID_INPUT,
+COMMENT_BODY_INPUT,
+COMMENT_DATE_INPUT);
+
+END$$
+
+DELIMITER ;
+
+USE `stockblogger`;
+DROP procedure IF EXISTS `edit_user`;
+
+DELIMITER $$
+USE `stockblogger`$$
+CREATE PROCEDURE `edit_user` (IN USER_ID_INPUT INT,IN USER_NAME_INPUT VARCHAR(45),IN DATE_JOINED_INPUT DATE,IN PASSWORD_INPUT VARCHAR(45))
+BEGIN
+
+UPDATE `stockblogger`.`users`
+SET user_name = USER_NAME_INPUT,
+	date_user_joined = DATE_JOINED_INPUT,
+    password = PASSWORD_INPUT
+WHERE user_id = USER_ID_INPUT;
+
+END$$
+
+DELIMITER ;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
