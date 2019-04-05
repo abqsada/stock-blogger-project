5d6f0317f9a5aa07e3790a38d01638a6b3f8c643
caleb <cschnarr85@gmail.com>
2019-04-05 09:57:19 -0600

    added method in data connection to query db for posts for a specified user
    Just a thought... would you rather search by user_name rather then _user id?? It kind of depends on how we are building the fron end which value will be easier to use.

--- Client-Server/DataConnection.java
+++ Client-Server/DataConnection.java
@@ -1,6 +1,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
 
 public class DataConnection {
 	
@@ -8,8 +10,18 @@ public class DataConnection {
 	private static final String PASSWORD = "user";
 	private static final String CONN = "jdbc:mysql://localhost/stockblogger";
 	
+	private static PreparedStatement stmt = null;
+	private static ResultSet rs = null;
+	private static Connection connection = null;
+	
 	public static Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(CONN, USERNAME, PASSWORD);
+		return connection = DriverManager.getConnection(CONN, USERNAME, PASSWORD);
+	}
+	
+	public static ResultSet getPostsForUser(String user_id) throws SQLException {
+		stmt = connection.prepareStatement("SELECT * FROM posts WHERE user_id = ?");
+		stmt.setString(1, user_id);
+		return rs = stmt.executeQuery();
 	}
 
 }
