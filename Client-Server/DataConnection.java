b1d55e8edf5865a959ae033aa8e9e048ba883ba9
caleb <cschnarr85@gmail.com>
2019-04-16 07:40:40 -0600

    Update DataConnection.java
    add methods to called stored procedures for add comment, add post, add user and get post for user

--- Client-Server/DataConnection.java
+++ Client-Server/DataConnection.java
@@ -1,4 +1,6 @@
+import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,7 +12,7 @@ public class DataConnection {
 	private static final String PASSWORD = "user";
 	private static final String CONN = "jdbc:mysql://localhost/stockblogger";
 	
-	private static PreparedStatement stmt = null;
+	private static CallableStatement stmt = null;
 	private static ResultSet rs = null;
 	private static Connection connection = null;
 	
@@ -18,10 +20,45 @@ public class DataConnection {
 		return connection = DriverManager.getConnection(CONN, USERNAME, PASSWORD);
 	}
 	
-	public static ResultSet getPostsForUser(String user_id) throws SQLException {
-		stmt = connection.prepareStatement("SELECT * FROM posts WHERE user_id = ?");
-		stmt.setString(1, user_id);
+	// method called to execute stored proc for select post from user. returns the result set
+	public static ResultSet getPostsForUser(int user_id) throws SQLException {
+		stmt = connection.prepareCall("{?= call select_posts_from_user(?)");
+		stmt.setInt(1, user_id);
 		return rs = stmt.executeQuery();
 	}
+	
+	// method called to execute the stored proc for adding a comment. returns the auto generated PK for comment id
+	public static int addComment(int post_id, int user_id, String body, String date) throws SQLException {
+		stmt = connection.prepareCall("{ call add_comment(?,?,?,?)");
+		stmt.setInt(1, post_id);
+		stmt.setInt(2, user_id);
+		stmt.setString(3, body);
+		stmt.setString(4, date);
+		stmt.executeUpdate();
+		rs = stmt.getGeneratedKeys();
+		return rs.getInt(1);
+	}
+	
+	public static int addPost(int user_id, String title, String body, Date date) throws SQLException {
+		stmt = connection.prepareCall("{ call add_comment(?,?,?,?)");
+		stmt.setInt(1, user_id);
+		stmt.setString(2, title);
+		stmt.setString(3, body);
+		stmt.setDate(4, date);
+		stmt.executeUpdate();
+		rs = stmt.getGeneratedKeys();
+		return rs.getInt(1);
+	}
+	
+	public static int addUser(String name, Date date, String password) throws SQLException {
+		stmt.setString(1, name);
+		stmt.setDate(2, date);
+		stmt.setString(3, password);
+		stmt.executeUpdate();
+		rs = stmt.getGeneratedKeys();
+		return rs.getInt(1);
+	}
+	
+	
 
 }
