25dbae06c14479f8eead6e1c568f9f037b2ccdc8
JamesTheRev13 <47574329+JamesTheRev13@users.noreply.github.com>
2019-04-23 08:57:32 -0600

    Imported HTTPClient

--- WebApp/StockBlogger/src/app/app.module.ts
+++ WebApp/StockBlogger/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -43,6 +44,7 @@ import { TickerSearchComponent } from './ticker-search/ticker-search.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     AppRoutingModule,
     CKEditorModule
