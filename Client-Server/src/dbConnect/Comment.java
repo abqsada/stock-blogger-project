1b1493a6d7e14dc4686a40d6c0255b1bbecdabae
comilehigh <48026149+comilehigh@users.noreply.github.com>
2019-05-02 09:41:28 -0600

    comments, tojson and constructor overloading
    added comments to user/post/comment classes for turn in. added tojson method for post and comment, overloaded the constructor for post and comment to create new java object from existing DB entity.

--- Client-Server/src/dbConnect/Comment.java
+++ Client-Server/src/dbConnect/Comment.java
@@ -1,17 +1,35 @@
+/* 
+ This is a class that represents a Comment entity as described in the database.
+ This class implements methods to retrieve a Comment from the database, edit a Comment in the database,
+ and creating a JSON object from the Comment object.
+ */
+
 package dbConnect;
 
 import java.sql.Date;
 import java.sql.SQLException;
 
+import com.google.gson.JsonObject;
+
 public class Comment {
 
+	// Instance variables to represent the different attributes of a Comment
 	private int commentId;
     private int postId;
     private int userId; // Change to: String userName in DB
     private String body;
     private Date commentDate;
 
-
+    // Constructor for creating an existing Comment object
+    public Comment(int commentId, int postID, int userID, String body, Date commentDate) throws SQLException{
+    	this.commentId = commentId;
+        this.postId = postID;
+        this.userId = userID;
+        this.body = body;
+        this.commentDate = commentDate;
+    }
+    
+    // Constructor for creating a new Comment and adding it to the database
     public Comment(int postID, int userID, String body, Date commentDate) throws SQLException{
         this.postId = postID;
         this.userId = userID;
@@ -20,6 +38,7 @@ public class Comment {
         this.commentId = DataConnection.addComment(postID, userID, body, commentDate);
     }
 
+    // Method for editing a Comment object and record in the database
     public void editComment(int postID, int userID, String body, Date commentDate) throws SQLException{
         this.postId = postID;
         this.userId = userID;
@@ -28,6 +47,8 @@ public class Comment {
         DataConnection.editComment(commentId, postID, userID, body, commentDate);
     }
 
+    // getters and setters for User object.
+ 	// setters update the database as well
     public int getPostID() {
         return postId;
     }
@@ -52,5 +73,16 @@ public class Comment {
     public void setCommentDate(Date commentDate) throws SQLException {
         this.commentDate = commentDate;
         editComment(postId, userId, body, commentDate);
+    }
+    
+    // method to create a JSON object from the Comment object
+    public JsonObject toJsonObj() {
+    	JsonObject job = new JsonObject();
+    	job.addProperty("commentId", this.commentId);
+    	job.addProperty("postId", this.postId);
+    	job.addProperty("userId", this.userId);
+    	job.addProperty("body",  this.body);
+    	job.addProperty("commentDate", this.commentDate.toString());
+        return job;
     }
 }
